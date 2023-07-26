b5e05a5e8347ba10eb1a9e87b528a5304a8c60ed
Shibin Shibu <shibinsb01@gmail.com>
2023-07-27 04:53:26 +0530

    learn

--- basics.py
+++ basics.py
@@ -1,1 +1,8 @@
 print("*"*10)
+name='Shibin'
+age=22
+student=True
+print(name,'|',age,"| Student:",student)
+name=input('Enter your Name: ')
+age=input('Enter your Age: ')
+print(name,' is ',age,' years old!')
