a91a9b1a733326e9632090d66287c4a416aa8f40
Shibin Shibu <shibinsb01@gmail.com>
2023-07-28 05:38:58 +0530

    string

--- str.py
+++ str.py
@@ -5,5 +5,9 @@ str3='Use single quotes to print "\n'
 str4='''This is a multi line string!
  This is cool!
  use three quotes.'''
+str5='Shibin'
 
 print(str,str1,str2,str3,str4)
+print(str5[4])
+print(str5[-4],str5[1:-1])
+
