f9ef9214201f9710575287946121ed3960079d0f
Shibin Shibu <shibinsb01@gmail.com>
2023-07-28 05:38:56 +0530

    string

---
+++ str.py
@@ -0,0 +1,9 @@
+str=' This is a string!\n'
+str1="This is also a string!\n"
+str2="Use double quotes to print '\n"
+str3='Use single quotes to print "\n'
+str4='''This is a multi line string!
+ This is cool!
+ use three quotes.'''
+
+print(str,str1,str2,str3,str4)
