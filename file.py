e3114b2eabb2c021ba6a20ccb112845a6e66f3da
Shibin Shibu <shibinsb01@gmail.com>
2023-09-25 04:13:32 +0530

    Change

--- file.py
+++ file.py
@@ -2,3 +2,5 @@ print("Hello World!!")
 name='Shibin'
 secName='Shibu'
 print(name+" "+secName+" Age:",int(input("Enter Age: ")))
+print("The Sum is: ",int(input("Enter 1st: "))+int(input("Enter 2st: ")))
+print("The Sum is: "+str(int(input("Enter 1st: "))+int(input("Enter 2st: "))))
